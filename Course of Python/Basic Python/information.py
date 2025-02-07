25043767053310079f8e3fadad7757180644b4ab
SebasCruz18 <juansecruz9999@gmail.com>
2025-02-07 18:54:16 -0500

    chore: delete SimpleData.py and helloworld.py

---
+++ Course of Python/Basic Python/information.py
@@ -0,0 +1,16 @@
+'''Despues de haer visto lo básico en python que son:
+- Primer "Hola Mundo" el que nunca falta a la hora de empezar con un lenguaje de programación.
+
+-FUNDAMENTOS
+
+ - Datos simples
+ - Variables
+ - Datos compuestos
+ - Operadores aritméticos
+ - Operadores de comparación
+ - Condicionales
+ - Operadores lógicos
+ - Metodos de Listas
+
+ Ya procedo a continuar con temas de nivel básico-intermedio.
+'''
