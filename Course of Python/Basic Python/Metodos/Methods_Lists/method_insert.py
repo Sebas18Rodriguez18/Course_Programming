f10beefb6034576c8289af238625187285f0e2c7
SebasCruz18 <sr1290853@gmail.com>
2025-02-07 18:40:26 -0500

    Add files via upload

---
+++ Course of Python/Basic Python/Metodos/Methods_Lists/method_insert.py
@@ -0,0 +1,13 @@
+#En este se mostrará el funcionamiento del metodo insert en una lista.
+
+list_number = [1,2,3,4,5,6,7,8,9,0]
+              #Creamos una lista con valores númericos enteros.
+print (list_number)
+print ()
+print("*************************************************************")
+print ()
+list_number.insert(5, "Apple")
+              #Con el metodo insert lo que hacemos es insertar o agregar un valor a 
+              #una lista en la posición que nosotros queramos.
+print (list_number)
+              #En este momento el valor "Apple" se agregará a la posición.
