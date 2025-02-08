c332aef3ebf5704a7e07afede95ddf73cd1e93c9
SebasCruz18 <juansecruz9999@gmail.com>
2025-02-07 19:17:17 -0500

    Add method_keys.py

---
+++ Course of Python/Metodos/Methods_Dictionarys/method_keys.py
@@ -0,0 +1,14 @@
+'''
+Este Metodo sirve para devolver las claves de nuestro diccionario
+tambien nos sirve para poder iterar.
+'''
+
+dictionary = {
+    "name" : "Sebas",
+    "lastname" : "Rodriguez",
+    "password" : 1234567890
+}
+
+method = dictionary.get()
+
+print(method)
