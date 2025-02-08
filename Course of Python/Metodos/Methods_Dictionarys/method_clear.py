37f7b6715f0a842654cd0330107c0aaee24724db
SebasCruz18 <juansecruz9999@gmail.com>
2025-02-07 19:17:17 -0500

    Add method_clear.py

---
+++ Course of Python/Metodos/Methods_Dictionarys/method_clear.py
@@ -0,0 +1,11 @@
+#Este Metodo sirve para eliminar todos los elementos de nuestro diccionario.
+
+dictionary = {
+    "name" : "Sebas",
+    "lastname" : "Rodriguez",
+    "password" : 1234567890
+}
+
+method = dictionary.clear()
+
+print(method)
