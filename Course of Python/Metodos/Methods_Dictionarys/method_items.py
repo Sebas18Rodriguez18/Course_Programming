e568839941061f4ce01f724a5ff0f886a999170f
SebasCruz18 <juansecruz9999@gmail.com>
2025-02-07 19:17:17 -0500

    Add method_items.py

---
+++ Course of Python/Metodos/Methods_Dictionarys/method_items.py
@@ -0,0 +1,11 @@
+#Este Metodo sirve para iterar el diccionario.
+
+dictionary = {
+    "name" : "Sebas",
+    "lastname" : "Rodriguez",
+    "password" : 1234567890
+}
+
+method = dictionary.items()
+
+print(method)
